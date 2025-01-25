9e17b45de0b771c80c6381f13de3088ac39facce
Daniel Montilla <montilla.d2000@gmail.com>
2025-01-25 00:47:42 -0400

    feat(core): add vite-tsconfig-paths support and update TypeScript configuration

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, UserConfig } from "vitest/config";
+
+type Plugin = NonNullable<UserConfig["plugins"]>[number];
 
 export default defineConfig({
+  plugins: [tsconfigPaths() as Plugin],
   test: {
     environment: "jsdom",
     globals: true,
