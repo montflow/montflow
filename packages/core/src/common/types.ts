b94dc2110dfa18cae2569a300f37e18aba21e0d0
Daniel Montilla <montilla.d2000@gmail.com>
2025-02-23 01:05:35 -0400

    feat(core): add `Lazy` and `Sync` types to common types

--- packages/core/src/common/types.ts
+++ packages/core/src/common/types.ts
@@ -1,6 +1,8 @@
 export type Optional<V> = V | undefined;
 export type Evaluable<V> = V | (() => V);
 export type Simplify<T> = { [KeyType in keyof T]: T[KeyType] } & {};
+export type Lazy<T> = () => T;
+export type Sync<T> = () => T;
 
 /**
  * @alias
