27bf8e9e8907ce034b614bdd33086b7a69908783
Daniel Montilla <montilla.d2000@gmail.com>
2025-09-19 23:18:04 -0400

    chore: remove svelte-reactive

--- packages/reactive-svelte/src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export * as Computed from "./computed/index.svelte.js";
-export * as Source from "./source/index.svelte.js";
-export * as State from "./state/index.svelte.js";
-export * as Watcher from "./watcher/index.svelte.js";
