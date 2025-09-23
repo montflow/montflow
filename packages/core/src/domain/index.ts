ab41a8f0590d0342ec0c370989138635bf9c0269
Daniel Montilla <montilla.d2000@gmail.com>
2025-09-23 15:39:26 -0400

    feat(core): complete `Domain` module

--- packages/core/src/domain/index.ts
+++ packages/core/src/domain/index.ts
@@ -1,2 +1,11 @@
-export const Tag = "_tag" as const;
+/**
+ * Standard key for "Id" property in custom structs.
+ * Used to identify the type of custom struct
+ */
 export const Id = "_id" as const;
+
+/**
+ * Standard key for "Tag" property in custom structs.
+ * Used to identify the variant of custom struct
+ */
+export const Tag = "_tag" as const;
