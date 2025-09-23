c6f3a65f827e121f8cf77dd02b33eb1629b563ca
Daniel Montilla <montilla.d2000@gmail.com>
2025-09-22 22:18:09 -0400

    feat(core): Complete `Alias` module

--- packages/core/src/alias/index.ts
+++ packages/core/src/alias/index.ts
@@ -1,5 +1,5 @@
 /**
- * Type alias for the global Promise type.
+ * Type alias for the Promise type.
  * @template T The type of the value that the Promise resolves to
  */
 export type Promise<T> = globalThis.Promise<T>;
