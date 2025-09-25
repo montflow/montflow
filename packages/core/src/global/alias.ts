9e7e9e5047b621411c30afd344a48139e8b846fc
Daniel Montilla <montilla.d2000@gmail.com>
2025-09-25 19:51:35 -0400

    feat(core): complete `global` module

---
+++ packages/core/src/global/alias.ts
@@ -0,0 +1,14 @@
+/**
+ * @alias null
+ */
+export type Missing = null;
+
+/**
+ * @alias true
+ */
+export type Yes = true;
+
+/**
+ * @alias false
+ */
+export type No = false;
--- packages/core/src/global/function.ts
+++ packages/core/src/global/function.ts
@@ -1,11 +1,19 @@
+import type * as Function from "../function/index.js";
+
 /**
- * @todo documentation
- * @todo testing
+ * @alias {@link Function.Lazy}
+ *
+ * Represents a lazy evaluation.
+ *
+ * @template T the type of the value
  */
 export type Lazy<T> = () => T;
 
 /**
- * @todo documentation
- * @todo testing
+ * @alias {@link Function.Nullary}
+ *
+ * Represents a synchronous task to produce a value of type `T`.
+ *
+ * @template T the type of the value
  */
 export type Sync<T> = () => T;
