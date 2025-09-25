919eabb4ff13c35a79cccfe027717c90175144c2
Daniel Montilla <montilla.d2000@gmail.com>
2025-09-25 19:52:25 -0400

    feat(core): complete `Iterable` module

--- packages/core/src/iterable/index.ts
+++ packages/core/src/iterable/index.ts
@@ -2,6 +2,5 @@
  * @todo documentation
  * @todo testing
  */
-export function isIterable<T>(value: unknown): value is Iterable<T> {
-  return value != null && typeof (value as any)[Symbol.iterator] === "function";
-}
+export const isIterable = <T>(value: unknown): value is Iterable<T> =>
+  value != null && typeof (value as any)[Symbol.iterator] === "function";
