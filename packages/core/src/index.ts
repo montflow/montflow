763cad0199ad1d25fd2e22e615b79368bc78b59d
Daniel Montilla <montilla.d2000@gmail.com>
2025-09-26 21:45:00 -0400

    feat(core): add Constructor module export to index

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,7 @@ export * as Alias from "./alias/index.js";
 export * as Array from "./array/index.js";
 export * as Async from "./async/index.js";
 export * as Chain from "./chain/index.js";
+export * as Constructor from "./constructor/index.js";
 export * as Function from "./function/index.js";
 export * as Iterable from "./iterable/index.js";
 export * as Macro from "./macro/index.js";
